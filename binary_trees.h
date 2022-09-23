4b49fd420d25363b5dbc2ef9e366cc61a6d0d06f
hanichris <hanichris71@gmail.com>
2022-09-23 10:17:34 -0700

    C function to that creates a new binary tree node. If a pointer is passed to the function as the parent pointer, then it serves as the parent node for the new node. Else, the new node serves as the root node of the binary tree. During the node's creation, it does not have any child nodes.

---
+++ binary_trees.h
@@ -0,0 +1,36 @@
+#ifndef _BINARY_TREES_H
+#define _BINARY_TREES_H
+
+#include <stddef.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+
+/* Define a Binary Search Tree */
+typedef struct binary_tree_s bst_t;
+
+/* Define an AVL tree */
+typedef struct binary_tree_s avl_t;
+
+/* Define a max binary heap */
+typedef struct binary_tree_s heap_t;
+
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif /* _BINARY_TREES_H */
