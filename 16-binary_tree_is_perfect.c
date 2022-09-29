6cb948205dc6d9c507496e358d8850ec2b97a7a5
hanichris <hanichris71@gmail.com>
2022-09-29 10:13:27 -0700

    C function to check whether a binary tree is perfect. In a perfect binary tree, the number of nodes is `2^h - 1`. Thus, we simply check if the value `nodes + 1` is a power of 2. This is done using a simple trick of `x & (x - 1) == 0` where in this case, we substitute `x` for `nodes + 1`. Therefore, the expression becomes `nodes & (nodes + 1) == 0`.

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+
+/**
+ * bt_size - measure the size of the binary tree.
+ * @tree: pointer to the root node of the tree to measure.
+ * Return: Tree size or 0 if NULL.
+ */
+size_t bt_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (1 + bt_size(tree->left) + bt_size(tree->right));
+}
+
+/**
+ * binary_tree_is_perfect - check if a binary tree is perfect.
+ * @tree: pointer to the root node of the tree to check.
+ * Return: Either 0 or 1.
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int nodes = (int)bt_size(tree);
+
+	return ((nodes & (nodes + 1)) == 0);
+}
